48c1c3c3328c4020459c44fb72b808c3f101e1e4
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-18 20:59:18 +0530

    added layout

--- Gemfile
+++ Gemfile
@@ -9,8 +9,8 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 
 
  group :development do
-   gem 'rspec-rails','2.0.0.beta.18'
+   gem 'rspec-rails','2.10.0'
  end
 group :test do
-      gem 'rspec','2.0.0.beta.18'
+      gem 'rspec','2.10.0'
  end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "returns http success" do
+      get 'home'
+      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample | Home" )
+  end
+	it "should have a non-blank body" do
+	get 'home'
+	response.body.should_not =~ /<body>\s*<\/body>/
+	end
+end
+
+  describe "GET 'contact'" do
+    it "returns http success" do
+      get 'contact'
+      response.should be_success
+    end
+  it "returns http" do
+      get 'contact'
+      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample | contacts" )
+  end
+end
+end
