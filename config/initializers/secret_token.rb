a7ebb4ae1688b66211684fbfacd54e16b5048fac
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-18 19:10:12 +0530

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoProj::Application.config.secret_token = '654559160e6c32877bbc97b2f7ff4dc37ff81cdf21c7bffbf62d95636825fa11cc7ce0b07fb95019a557057f9740bc465f1069be1ebb93ed350e71a86bbe6f71'
