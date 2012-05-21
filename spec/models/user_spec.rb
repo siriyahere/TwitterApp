6aaff186c9469528cfd410fea4eaf795b83c5f3d
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-21 22:32:10 +0530

    Added pwd and encrypted pwd

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
 
 	before(:each) do
-		@attr= { :name => "Sample" , :email => "user@sample.com"}
+		@attr= { :name => "Sample" , :email => "user@sample.com", :password=>"foobar", :password_confirmation => "foobar"}
 
  # pending "add some examples to (or delete) #{__FILE__}"
  it "new instance shud be created" do
@@ -47,8 +47,45 @@ it "shud reject upto case" do
     user_with_dupl=User.new(@attr)	
     	user_with_dupl.should_not be_valid
     end
+
+    describe "passwords" do
+       before (:each) do
+       	@user= User.new(@attr)
+       end
+    	it "should have pwd attribute" do
+    		@user.should respond_to(:password)
+		end
+		it "should have pwd confirmation attribute" do
+    		@user.should respond_to(:password_confirmation)
+		end		
+    end
+    describe "password validations" do
+    	it "shud require a pwd" do
+    		User.new(@attr.merge(:password=> "", :password_confirmation => "")).should_not_be_valid
+    	end
+    	it "shud require a matching pwd" do
+    		User.new(@attr.merge(:password_confirmation=> "invalid")).should_not_be_valid
+    	end
+     
+     	it "should reject short passwords" do
+     	    short ="abcde" 
+     	    hash=@attr.merge(:password => short,:password_confirmation=>short)
+ 			User.new(hash).should_not be_valid
+ 		end 
+ 		it "should reject long passwords" do
+     	    short ="a"*41 
+     	    hash=@attr.merge(:password => short,:password_confirmation=>short)
+ 			User.new(hash).should_not be_valid
+ 		end   
+
+    end
+    describe "password encryption" do
+    	before(:each) do
+    		@user=User.create!(@attr)
+    	end
+    	it "should have an encrypted pwd  attribute" do
+    		@user.should respond_to(:encrypted_password)
+		end		
+
 end
-
-
-
 end
