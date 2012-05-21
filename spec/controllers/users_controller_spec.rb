1f63360992f59246799a039d6805f764f1b2f7a1
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-22 00:59:13 +0530

    Added gravatar

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -28,6 +28,11 @@ describe UsersController do
       get :show, :id => @user
       response.should have_selector('h1>img', :class => "gravatar")
   end
+  it "should have the right URL" do
+    get :show , :id=> @user
+    response.should have_selector('td>a', :content => user_path(@user),
+  end                                        :href => user_path(@user))
+
 end
   
   describe "GET 'new'" do
