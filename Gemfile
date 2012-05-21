17f45173c050d70abe3bea3bbf0da09d007c8f49
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-22 00:02:49 +0530

    Added Factory

--- Gemfile
+++ Gemfile
@@ -14,4 +14,5 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
  end
 group :test do
       gem 'rspec','2.10.0'
+      gem 'factory_girl_rails' , '1.0'
  end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name "Siriya"
+	user.email "siriya@sample.com"
+	user.password "foobar"
+	user.password_confirmation "foobar"
+end	
