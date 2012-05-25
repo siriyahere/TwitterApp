ecf2b49f14cdd667a2069b40743ae40c518be112
siriya <maluchari@gmail.com>
2012-05-25 19:45:04 +0530

    Fianl _commit

--- app/models/retweet.rb
+++ app/models/retweet.rb
@@ -6,7 +6,9 @@ class Retweet < ActiveRecord::Base
 
 private
  def self.followed_by (user)
+   
    followed_ids = %(SELECT micropost_id FROM  retweets WHERE user_id = :user_id)
+  
   Micropost.where("id IN (#{followed_ids})", :user_id => user.id)
  end
 end
